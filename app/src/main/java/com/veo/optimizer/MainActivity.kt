a3ec9f89392f07044c341e5d26eb1a1ceabcd0ac
cihanbay <84873245+cihanbay@users.noreply.github.com>
2026-06-29 11:53:19 +0330

    Fix: add ExperimentalMaterial3Api opt-in to AppRoot

--- app/src/main/java/com/veo/optimizer/MainActivity.kt
+++ app/src/main/java/com/veo/optimizer/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -35,6 +36,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppRoot() {
     val vm: AppViewModel = viewModel()
